3d888bc59ba960a329a80b712c3d797d1e71f2d3
King Chen <king.chen@bahwancybertek.com>
2020-07-31 15:30:22 +0800

    add android demo app version code

--- androidDemo/app/src/main/java/com/king/usingrn/FirstFragment.java
+++ androidDemo/app/src/main/java/com/king/usingrn/FirstFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -24,6 +25,9 @@ public class FirstFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        TextView versionName = view.findViewById(R.id.text_version);
+        versionName.setText("version : " + BuildConfig.VERSION_NAME + "(" + BuildConfig.VERSION_CODE + ")");
 
         view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
             @Override
