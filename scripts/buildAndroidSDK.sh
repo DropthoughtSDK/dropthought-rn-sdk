a1d08f42835b22bb4b7853a9ce37fd8735181119
BCT-Barney <barney.chen@bahwancybertek.com>
2021-04-12 14:20:20 +0800

    Bump Android SDK version to 1.1.8 Upgrade Android SDK version to 1.1.8 for androidDemo Update buildAndroidSDK

--- scripts/buildAndroidSDK.sh
+++ scripts/buildAndroidSDK.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
+echo "...build js part"
 mkdir -p android/dt-sdk/src/main/assets
 mkdir -p android/dt-sdk/src/main/res
 rm -rf android/dt-sdk/src/main/assets/index.android.bundle
 npx react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/dt-sdk/src/main/assets/index.android.bundle --assets-dest android/dt-sdk/src/main/res
-
-cd android;
+cd android
+echo "...gradle clean"
 ./gradlew clean dt-sdk:assembleRelease
+echo "...publishToMavenLocal"
 ./gradlew dt-sdk:publishToMavenLocal
+open ../package.json
+echo "Change version in package.json before publish Android SDK to s3"
+read -p "Press enter to continue"
+./gradlew publish
